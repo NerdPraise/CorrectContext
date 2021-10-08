45a67f3e4d0b9e516564333f7df5176671ce186e
NerdPraise <praiseajayi2@gmail.com>
2021-10-08 18:32:26 +0100

    Update view to validate request data

--- context_api/views.py
+++ context_api/views.py
@@ -8,9 +8,35 @@ from .serializers import ReportSerializer
 class ReportAPIView(APIView):
     serializer_class = ReportSerializer
 
+    def validate_data(self, data):
+        schedule = data.get("schedule")
+        if schedule == "NoRepeat":
+            data["day"] = None
+            data["time"] = None
+            data["date"] = None
+            is_valid = True
+        elif schedule == "Specific":
+            is_valid = bool(data.get("date") and data.get("time"))
+        elif schedule == "Daily":
+            is_valid = bool(data.get("time"))
+            data["day"] = None
+            data["date"] = None
+        elif schedule == "Weekly":
+            is_valid = bool(data.get("day") and data.get("time"))
+            data["date"] = None
+        else:
+            is_valid = False
+
+        return is_valid, data
+
     def post(self, request):
         data = request.data
+        is_valid, data = self.validate_data(data)
         serializer = self.serializer_class(data=data)
+        if not is_valid:
+            return Response(
+                {"message": "Invalid data"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
         if serializer.is_valid():
             serializer.save()
